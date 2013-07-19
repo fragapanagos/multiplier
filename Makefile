64f7a20109a31d7a94502fe65cf777943e288c7a
samfok <sam.b.fok@gmail.com>
2013-07-19 11:38:27 -0700

    add timing functions

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@
 CC = gcc
 
 # use CFLAGS to set compile flags
-# -g        compile with debug information
-# -Wall     display all warnings
-# -O0       use optimization level 0
-# -std=c99  use the C99 standard language definition
-CFLAGS = -g -Wall -O0 -std=c99
+# -g            compile with debug information
+# -Wall         display all warnings
+# -O0           use optimization level 0
+# -std=gnu99    use the C99 standard language definition with gnu extensions
+CFLAGS = -g -Wall -O0 -std=gnu99
 
 # use LDFLAGS to set linker flags
-# -lm       link to libm (math library)
-# LDFLAGS = -lm
+# -lrt      link to librt (real time library)
+LDFLAGS = -lrt
 
 # list project files
 SOURCES = mult.c
@@ -18,7 +18,7 @@ OBJECTS = $(SOURCES:.c=.o)
 TARGET = mult
 
 $(TARGET) : $(OBJECTS)
-	$(CC) $(CFLAGS) -o $@ $^ 
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 .PHONY: clean
 
--- mult.c
+++ mult.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <time.h>
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
-#define INTBITS (sizeof(unsigned int) * 8) // assume we know number of bits in integer representation
+#define BITS_IN_INT (sizeof(unsigned int) * 8) // assume we know number of bits in integer representation
 
+// Finds the difference between two times.
+void diff(struct timespec *start, struct timespec *end, struct timespec *result)
+{
+   if ((end->tv_nsec - start->tv_nsec)<0) { // need to carry subtraction
+      result->tv_sec = end->tv_sec - start->tv_sec - 1;
+      result->tv_nsec = end->tv_nsec + 1000000000 - start->tv_nsec;
+   } else {
+      result->tv_sec = end->tv_sec - start->tv_sec;
+      result->tv_nsec = end->tv_nsec - start->tv_nsec;
+   }
+}
+
+// Times a function used to implement multiplication.
+void time_multiply(int (*multiply_func)(int, int), const char *description) 
+{
+   int num_iter = 1000;
+   struct timespec time1, time2, time_diff;
+
+   printf("\nTiming implementation using %s\n", description);
+   printf("n\ttime(s) to compute n^2\n");
+   for (int i = 1; i < 40000; i*=2) {
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+      for (int j = 0; j<num_iter; j++) {
+         multiply_func(i, i);
+      }
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+      diff(&time1, &time2, &time_diff);
+      printf("%d \t%lld.%.9ld\n", i, (long long)time_diff.tv_sec, time_diff.tv_nsec);
+   }
+}
+
+// Implements multiplication using repeated addition.
 int multiply_by_adds(int m, int n)
 {
    int iterations = MIN(m, n);
@@ -15,17 +48,20 @@ int multiply_by_adds(int m, int n)
    return result;
 }
 
+// Implements multiplication using addition and bit shifting.
 int multiply_by_shifts_adds(int m, int n)
 {
    int multiplier = MIN(m, n);
    int base = MAX(m, n);
    int result = 0;
-   for (int i = 0; i < sizeof(unsigned int) * 8 ; i++) {
+   for (int i = 0; i < BITS_IN_INT; i++) {
       if (0x1 << i & multiplier) result += base << i;
    }
    return result;
 }
 
+// Implements multiplication using addition and bit shifting.
+// Works only over the necessary number of bits
 int multiply_by_shifts_adds_opt(int m, int n)
 {
    int multiplier = MIN(m, n);
@@ -34,7 +70,7 @@ int multiply_by_shifts_adds_opt(int m, int n)
 
    if (multiplier == 0) return 0;
    int lead_zeros = __builtin_clz(multiplier); // builtin gcc function
-   int highest_pow = INTBITS - lead_zeros - 1; 
+   int highest_pow = BITS_IN_INT - lead_zeros - 1; 
 
    for (int i = highest_pow; i >= 0 ; i--) {
       if (0x1 << i & multiplier) result += base << i;
@@ -44,7 +80,14 @@ int multiply_by_shifts_adds_opt(int m, int n)
 
 int main()
 {
+   /* Comparing the time of each implementation */
+   // time_multiply(multiply_by_adds, "adds");
+   // time_multiply(multiply_by_shifts_adds, "shifts and adds");
+   // time_multiply(multiply_by_shifts_adds_opt, "shifts and adds, opt");
+
    int m, n;
+   m = 10;
+   n = 10;
    printf("Enter two values to multiply together.\n");
    scanf("%d%d", &m, &n);
    
@@ -66,7 +109,10 @@ int main()
    if (sign == -1) {
       prod_adds = -prod_adds;
       prod_shifts_adds = -prod_shifts_adds;
+      prod_shifts_adds_opt = -prod_shifts_adds_opt;
    }
+
+   /* For checking that product is implemented correctly */
    printf("Using repeated adds implementation, product is \t\t\t%d\n", prod_adds);
    printf("Using bitshift adds implementation, product is \t\t\t%d\n", prod_shifts_adds);
    printf("Using optimized bitshift adds implementation, product is \t%d\n", prod_shifts_adds_opt);
